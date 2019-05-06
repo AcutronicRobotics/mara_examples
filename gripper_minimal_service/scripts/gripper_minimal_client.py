c8cca3fc4b8fa689cc3a14ef9b18ddacef81df20
alesolano <asolanopf@gmail.com>
2019-05-06 12:57:06 +0200

    update service client with python3

--- gripper_minimal_service/scripts/gripper_minimal_client.py
+++ gripper_minimal_service/scripts/gripper_minimal_client.py
@@ -1,20 +1,35 @@
-#ROS 2.0
-from hrim_actuator_gripper_srvs.srv import ControlFinger
+#!/usr/bin/python3
+
 import rclpy
+from rclpy.qos import qos_profile_services_default
+from hrim_actuator_gripper_srvs.srv import ControlFinger
+
+# -------- #
 
 rclpy.init(args=None)
-node = rclpy.create_node('test_finger_control_service')
 
-cli = node.create_client(ControlFinger, '/hros_actuation_gripper_000000000004/goal')
+# Create Node with name "mara_minimal_client"
+node = rclpy.create_node("mara_minimal_client")
 
+# Create a client for service "/hrim_actuation_gripper_000000000004/goal"
+client = node.create_client(ControlFinger, "/hrim_actuation_gripper_000000000004/goal", qos_profile=qos_profile_services_default)
+
+# Create request with the same type as the service, ControlFinger
 req = ControlFinger.Request()
 
-req.goal_velocity = 30.0         # velocity range: 30 -  250 mm/s
-req.goal_effort = 10.0        # forces range:   10 -  125 N
-req.goal_angularposition = 0.87  # position range   0 - 0.87 rad
+# Position range 0 - 0.87 rad
+# TODO: The range is wider for me (up to 9.0 sometimes)
+req.goal_linearposition = 0.87
 
-future = cli.call_async(req)
+# Wait for service to be avaiable before calling it
+while not client.wait_for_service(timeout_sec=1.0):
+   node.get_logger().info('Service not available, waiting again...')
+
+# Call service and spin
+future = client.call_async(req)
 rclpy.spin_until_future_complete(node, future)
+
+# Analyze the result
 if future.result() is not None:
     node.get_logger().info('Goal accepted: %d: ' % future.result().goal_accepted)
 else:
