3033dbd2d0da2588c9bc0f03d7c271a5643d6cfa
alesolano <asolanopf@gmail.com>
2019-05-06 18:11:51 +0200

    add new version of cpp client

---
+++ gripper_minimal_service/src/gripper_minimal_client_v2.cpp
@@ -0,0 +1,56 @@
+#include "rclcpp/rclcpp.hpp"
+#include "hrim_actuator_gripper_srvs/srv/control_finger.hpp"
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+class MinimalClient : public rclcpp::Node
+{
+  public:
+    MinimalClient(): Node("gripper_minimal_client")
+    {
+      client = this->create_client<hrim_actuator_gripper_srvs::srv::ControlFinger>(
+        "/hrim_actuation_gripper_000000000004/goal");
+
+      // Wait for service to be avaiable before calling it
+      while (!client->wait_for_service(1s)) {
+        RCLCPP_INFO(this->get_logger(), "Service not available, waiting again...");
+      }
+
+      request = std::make_shared<hrim_actuator_gripper_srvs::srv::ControlFinger::Request>();
+    }
+
+    void send_request();
+    rclcpp::Client<hrim_actuator_gripper_srvs::srv::ControlFinger>::SharedFuture future;
+
+  private:
+    rclcpp::Client<hrim_actuator_gripper_srvs::srv::ControlFinger>::SharedPtr client;
+    rclcpp::Client<hrim_actuator_gripper_srvs::srv::ControlFinger>::SharedRequest request;
+};
+
+void MinimalClient::send_request(){
+  request->goal_linearposition = 0.87;
+  this->future = client->async_send_request(request);
+}
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<MinimalClient>();
+
+  node->send_request();
+  rclcpp::spin_until_future_complete(node, node->future);
+
+  // Analyze the result
+  auto result = node->future.get();
+  if (result) {
+    RCLCPP_INFO(node->get_logger(), "Result of gripper_minimal_client: %d", result->goal_accepted);
+  } else {
+    RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for response. Exiting.");
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}
