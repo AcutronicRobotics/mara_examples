b27e44828d89b0a0ed62bdc7fc09f235a3aa749e
alesolano <asolanopf@gmail.com>
2019-05-06 13:37:02 +0200

    update service client with cpp

--- gripper_minimal_service/src/gripper_minimal_client.cpp
+++ gripper_minimal_service/src/gripper_minimal_client.cpp
@@ -1,81 +1,37 @@
-#include <chrono>
-#include <iostream>
-#include <memory>
-#include <string>
-
 #include "rclcpp/rclcpp.hpp"
-#include "rcutils/cmdline_parser.h"
-
 #include "hrim_actuator_gripper_srvs/srv/control_finger.hpp"
+#include <chrono>
 
 using namespace std::chrono_literals;
 
-void print_usage()
-{
-  printf("Usage for gripper_minimal_client app:\n");
-  printf("gripper_minimal_client [-p position value] [-t topic] [-h]\n");
-  printf("options:\n");
-  printf("-h : Print this help function.\n");
-  printf("-p position value : Position of the fingers.\n");
-  printf("-t topic : Name of the service.\n");
-}
-
-hrim_actuator_gripper_srvs::srv::ControlFinger::Response::SharedPtr send_request(
-  rclcpp::Node::SharedPtr node,
-  rclcpp::Client<hrim_actuator_gripper_srvs::srv::ControlFinger>::SharedPtr client,
-  hrim_actuator_gripper_srvs::srv::ControlFinger::Request::SharedPtr request)
-{
-  auto result = client->async_send_request(request);
-  // Wait for the result.
-  if (rclcpp::spin_until_future_complete(node, result) ==
-    rclcpp::executor::FutureReturnCode::SUCCESS)
-  {
-    return result.get();
-  } else {
-    return NULL;
-  }
-}
-
 int main(int argc, char ** argv)
 {
-  // Force flush of the stdout buffer.
-  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
   rclcpp::init(argc, argv);
 
-  auto node = rclcpp::Node::make_shared("hros_actuator_gripper_srvs");
+  // Create Node with name "mara_minimal_client"
+  auto node = rclcpp::Node::make_shared("mara_minimal_client");
 
-  if (rcutils_cli_option_exist(argv, argv + argc, "-h")) {
-    print_usage();
-    return 0;
-  }
+  // Create a client for service "/hrim_actuation_gripper_000000000004/goal"
+  auto client = node->create_client<hrim_actuator_gripper_srvs::srv::ControlFinger>("/hrim_actuation_gripper_000000000004/goal");
 
-  float position = 0.0;
-  char * cli_option = rcutils_cli_get_option(argv, argv + argc, "-p");
-  if (nullptr != cli_option) {
-    position = std::atof(cli_option);
-  }
-
-  std::string topic("goal");
-  char * cli_option_topic = rcutils_cli_get_option(argv, argv + argc, "-t");
-  if (nullptr != cli_option_topic) {
-    topic = std::string(cli_option_topic);
-  }
-
-  auto client = node->create_client<hrim_actuator_gripper_srvs::srv::ControlFinger>(topic);
-
+  // Create request with the same type as the service, ControlFinger
   auto request = std::make_shared<hrim_actuator_gripper_srvs::srv::ControlFinger::Request>();
-  request->goal_angularposition = position;
 
+  // Position range 0 - 0.87 rad
+  request->goal_linearposition = 0.87;
+
+  // Wait for service to be avaiable before calling it
   while (!client->wait_for_service(1s)) {
-    if (!rclcpp::ok()) {
-      RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service. Exiting.");
-      return 0;
-    }
-    RCLCPP_INFO(node->get_logger(), "service not available, waiting again...");
+    RCLCPP_INFO(node->get_logger(), "Service not available, waiting again...");
   }
 
-  auto result = send_request(node, client, request);
+  // Call service and spin
+  auto future = client->async_send_request(request);
+  rclcpp::spin_until_future_complete(node, future);
+
+  // Analyze the result
+  auto result = future.get();
   if (result) {
     RCLCPP_INFO(node->get_logger(), "Result of gripper_minimal_client: %d", result->goal_accepted);
   } else {
