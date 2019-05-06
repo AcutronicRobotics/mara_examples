39e0d4ceef6e35bd53e9faecc73ed7298c50019b
alesolano <asolanopf@gmail.com>
2019-05-06 17:05:59 +0200

    add new version of cpp subscriber

--- mara_minimal_subscriber/src/mara_minimal_subscriber_v1.cpp
+++ mara_minimal_subscriber/src/mara_minimal_subscriber_v1.cpp
@@ -2,7 +2,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "hrim_actuator_rotaryservo_msgs/msg/state_rotary_servo.hpp"
 
-void minimalCallback(const hrim_actuator_rotaryservo_msgs::msg::StateRotaryServo::UniquePtr msg)
+// Function that will be called once a message is published to the topic we are subscribed
+void minimal_callback(const hrim_actuator_rotaryservo_msgs::msg::StateRotaryServo::UniquePtr msg)
 {
   std::cout << "Position: " << msg->position << std::endl;
 }
@@ -11,11 +12,13 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
 
+  // Create Node with name "mara_minimal_subscriber"
   auto node = rclcpp::Node::make_shared("mara_minimal_subscriber");
 
+  // Subscribe to topic "/hrim_actuation_servomotor_000000000001/state_axis1" and link it to "minimal_callback" function
   auto sub = node->create_subscription<hrim_actuator_rotaryservo_msgs::msg::StateRotaryServo>(
     "/hrim_actuation_servomotor_000000000001/state_axis1",
-    minimalCallback,
+    minimal_callback,
     rmw_qos_profile_sensor_data); // QoS profile for reading (joint) sensors
 
   // Spin listening to all subscribed topics
