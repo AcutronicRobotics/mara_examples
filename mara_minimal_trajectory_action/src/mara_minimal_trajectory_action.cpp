19e8c712293c9d6ed61a00ff8d4cb184ba3ffea8
alesolano <asolanopf@gmail.com>
2019-06-11 18:14:59 +0200

    update action to dashing

--- mara_minimal_trajectory_action/src/mara_minimal_trajectory_action.cpp
+++ mara_minimal_trajectory_action/src/mara_minimal_trajectory_action.cpp
@@ -1,29 +1,24 @@
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "hrim_actuator_rotaryservo_actions/action/goal_joint_trajectory.hpp"
+
 #include <inttypes.h>
 #include <memory>
 
-// ROS 2.0
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
-#include "rcutils/cmdline_parser.h"
+using HRIMJointTrajectory = hrim_actuator_rotaryservo_actions::action::GoalJointTrajectory;
+using GoalHandleHRIMJointTrajectory = rclcpp_action::ClientGoalHandle<HRIMJointTrajectory>;
 
-// HRIM
-#include "hrim_actuator_rotaryservo_actions/action/goal_joint_trajectory.hpp"
-
-rclcpp::Node::SharedPtr g_node = nullptr;
-
-void print_usage()
-{
-  printf("Usage for gripper_minimal_client app::\n");
-  printf("ros2 run mara_minimal_trajectory_action mara_minimal_trajectory_action [-t topic] [-h]\n");
-  printf("options:\n");
-  printf("-h : Print this help function.\n");
-  printf("-t action name : Name of the action.\n");
-}
+// node as global variable
+rclcpp::Node::SharedPtr node = nullptr;
 
 void feedback_callback(
-  rclcpp_action::ClientGoalHandle<hrim_actuator_rotaryservo_actions::action::GoalJointTrajectory>::SharedPtr,
-  const std::shared_ptr<const hrim_actuator_rotaryservo_actions::action::GoalJointTrajectory::Feedback> feedback)
+  GoalHandleHRIMJointTrajectory::SharedPtr,
+  const std::shared_ptr<const HRIMJointTrajectory::Feedback> feedback)
 {
+  //
+  // ----------- FEEDBACK ----------- //
+  //
+
   double time_from_start = (double)(feedback->actual.time_from_start.sec) +
                            (double)(feedback->actual.time_from_start.nanosec/1e+9);
   double time_from_start_desired = (double)(feedback->desired.time_from_start.sec) +
@@ -31,121 +26,136 @@ void feedback_callback(
   double time_from_start_error = (double)(feedback->error.time_from_start.sec) +
                                  (double)(feedback->error.time_from_start.nanosec/1e+9);
 
-  RCLCPP_INFO(g_node->get_logger(), "Current degrees: %.2f\ttime:%.5f",
+  RCLCPP_INFO(node->get_logger(), "Current degrees: %.2f\ttime:%.5f",
                                         feedback->actual.positions[0]*180/3.1416,
                                         time_from_start);
-  RCLCPP_INFO(g_node->get_logger(), "Desired degrees: %.2f\ttime:%.5f",
+
+  // TODO: Here program is shutdown. Probably because of spin_until_future_complete
+  RCLCPP_INFO(node->get_logger(), "Desired degrees: %.2f\ttime:%.5f",
                                         feedback->desired.positions[0]*180/3.1416,
                                         time_from_start_desired);
-  RCLCPP_INFO(g_node->get_logger(), "Error degrees: %.2f\ttime:%.5f",
+  RCLCPP_INFO(node->get_logger(), "Error degrees: %.2f\ttime:%.5f",
                                         feedback->error.positions[0]*180/3.1416,
                                         time_from_start_error);
-  RCLCPP_INFO(g_node->get_logger(), "-----------------------------");
+  RCLCPP_INFO(node->get_logger(), "-----------------------------");
 }
+
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
 
-  if (rcutils_cli_option_exist(argv, argv + argc, "-h")) {
-    print_usage();
-    return 0;
-  }
-
-  std::string action_name("/trajectory");
-  if (rcutils_cli_option_exist(argv, argv + argc, "-topic")){
-    action_name = rcutils_cli_get_option(argv, argv + argc, "-topic");
-  }
-
+  std::string action_name("/hrim_actuator_rotaryservo_000000000001/trajectory_axis1");
   printf("Trying to connect with the action %s\n", action_name.c_str());
 
-  g_node = rclcpp::Node::make_shared("minimal_action_client");
-  auto action_client = rclcpp_action::create_client<hrim_actuator_rotaryservo_actions::action::GoalJointTrajectory>(g_node, action_name);
+  node = rclcpp::Node::make_shared("minimal_action_client");
+  auto action_client = rclcpp_action::create_client<HRIMJointTrajectory>(node, action_name);
+
+
+  //
+  // ----------- SEND GOAL ----------- //
+  //
 
   if (!action_client->wait_for_action_server(std::chrono::seconds(20))) {
-    RCLCPP_ERROR(g_node->get_logger(), "Action server not available after waiting");
+    RCLCPP_ERROR(node->get_logger(), "Action server not available after waiting");
     return 1;
   }
 
   // Populate a goal
-  auto goal_msg = hrim_actuator_rotaryservo_actions::action::GoalJointTrajectory::Goal();
+  auto goal_msg = HRIMJointTrajectory::Goal();
   goal_msg.trajectory.joint_names.push_back("motor1");
 
+  // Define point1 in pos=0 at time=0
   trajectory_msgs::msg::JointTrajectoryPoint point1;
   point1.positions.push_back(0);
   point1.velocities.push_back(0);
   point1.time_from_start.sec = 0;
   point1.time_from_start.nanosec = 0;
-  goal_msg.trajectory.points.push_back(point1);
 
+  // Define point2 in pos=0.2 at time=5
   trajectory_msgs::msg::JointTrajectoryPoint point2;
-  point2.positions.push_back(1.57);
+  point2.positions.push_back(0.5);
   point2.velocities.push_back(0);
-  point2.time_from_start.sec = 5; // velocity 1.57rad/5s = 0.314 rad/s
+  point2.time_from_start.sec = 5; // velocity 0.5rad/5s = 0.1 rad/s
   point2.time_from_start.nanosec = 0;
-  goal_msg.trajectory.points.push_back(point2);
 
+  // Define point3 in pos=0 at time=7.5
   trajectory_msgs::msg::JointTrajectoryPoint point3;
   point3.positions.push_back(0);
   point3.velocities.push_back(0);
-  point3.time_from_start.sec = 10; // velocity 1.57rad/(10s-5s) = 0.314 rad/s
-  point3.time_from_start.nanosec = 0;
+  point3.time_from_start.sec = 7; // velocity 0.5rad/(7.5s-5s) = 0.2 rad/s
+  point3.time_from_start.nanosec = 5e8;
+
+  // Add points to goal_msg
+  goal_msg.trajectory.points.push_back(point1);
+  goal_msg.trajectory.points.push_back(point2);
   goal_msg.trajectory.points.push_back(point3);
 
-  RCLCPP_INFO(g_node->get_logger(), "Sending goal");
-  // Ask server to achieve some goal and wait until it's accepted
-  auto goal_handle_future = action_client->async_send_goal(goal_msg, feedback_callback, true);
+  RCLCPP_INFO(node->get_logger(), "Sending goal");
+  // Ask server to achieve some goal. Assign feedback callback
+  rclcpp_action::Client<HRIMJointTrajectory>::SendGoalOptions options = rclcpp_action::Client<HRIMJointTrajectory>::SendGoalOptions();
+  options.feedback_callback = feedback_callback;
+  auto goal_handle_future = action_client->async_send_goal(goal_msg, options);
 
-  if (rclcpp::spin_until_future_complete(g_node, goal_handle_future) !=
+  if (rclcpp::spin_until_future_complete(node, goal_handle_future) !=
     rclcpp::executor::FutureReturnCode::SUCCESS)
   {
-    RCLCPP_ERROR(g_node->get_logger(), "send goal call failed :(");
+    RCLCPP_ERROR(node->get_logger(), "`send_goal` call failed");
     return 1;
   }
 
-  rclcpp_action::ClientGoalHandle<hrim_actuator_rotaryservo_actions::action::GoalJointTrajectory>::SharedPtr goal_handle = goal_handle_future.get();
+
+  //
+  // ----------- GOAL RESPONSE ----------- //
+  //
+
+  GoalHandleHRIMJointTrajectory::SharedPtr goal_handle = goal_handle_future.get();
   if (!goal_handle) {
-    RCLCPP_ERROR(g_node->get_logger(), "Goal was rejected by server");
+    RCLCPP_ERROR(node->get_logger(), "Goal was rejected by server");
     rclcpp::shutdown();
     return 0;
   }
 
-  printf("get_status %d\n", goal_handle->get_status());
-
-  bool action_server_is_ready = action_client->action_server_is_ready();
-  printf("action_server_is_ready: %d\n", action_server_is_ready);
-
-  std::cout << "get_goal_stamp " << goal_handle->get_goal_stamp().seconds() << std::endl;
+  //
+  // ----------- GET RESULT ----------- //
+  //
 
   // Wait for the server to be done with the goal
   auto result_future = action_client->async_get_result(goal_handle);
 
-  RCLCPP_INFO(g_node->get_logger(), "Waiting for result");
-  if (rclcpp::spin_until_future_complete(g_node, result_future) !=
+  RCLCPP_INFO(node->get_logger(), "Waiting for result");
+
+  if (rclcpp::spin_until_future_complete(node, result_future) !=
     rclcpp::executor::FutureReturnCode::SUCCESS)
   {
-    RCLCPP_ERROR(g_node->get_logger(), "get result call failed :(");
+    RCLCPP_ERROR(node->get_logger(), "`get_result` call failed");
     return 1;
   }
 
-  rclcpp_action::ClientGoalHandle<hrim_actuator_rotaryservo_actions::action::GoalJointTrajectory>::Result result = result_future.get();
+  // TODO: Here program is shutdown. Probably because of spin_until_future_complete
+
+  GoalHandleHRIMJointTrajectory::WrappedResult result = result_future.get();
 
   switch(result.code) {
     case rclcpp_action::ResultCode::SUCCEEDED:
+      RCLCPP_INFO(node->get_logger(), "Goal succeeded");
       break;
     case rclcpp_action::ResultCode::ABORTED:
-      RCLCPP_ERROR(g_node->get_logger(), "Goal was aborted");
+      RCLCPP_ERROR(node->get_logger(), "Goal was aborted");
       return 1;
     case rclcpp_action::ResultCode::CANCELED:
-      RCLCPP_ERROR(g_node->get_logger(), "Goal was canceled");
+      RCLCPP_ERROR(node->get_logger(), "Goal was canceled");
       return 1;
     default:
-      RCLCPP_ERROR(g_node->get_logger(), "Unknown result code");
+      RCLCPP_ERROR(node->get_logger(), "Unknown result code");
       return 1;
   }
 
+
+
+
   action_client.reset();
-  g_node.reset();
+  node.reset();
   rclcpp::shutdown();
   return 0;
 }
